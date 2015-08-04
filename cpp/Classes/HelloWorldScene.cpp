7dc209ba7dea2b6b6ea0d4f77623f446a0f88416
Nite Luo <darkdukey@gmail.com>
2015-08-04 16:06:02 -0700

    Finish integration

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -1,6 +1,12 @@
 #include "HelloWorldScene.h"
+#if COCOS2D_VERSION >= 196608
+#include "ui/CocosGUI.h"
+#endif
 
 USING_NS_CC;
+using namespace sdkbox;
+
+template <typename T> std::string tostr(const T& t) { std::ostringstream os; os<<t; return os.str(); }
 
 Scene* HelloWorld::createScene()
 {
@@ -9,10 +15,10 @@ Scene* HelloWorld::createScene()
     
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -26,61 +32,172 @@ bool HelloWorld::init()
     {
         return false;
     }
+    int winWidth = CCDirector::getInstance()->getWinSize().width;
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    IAP::setDebug(true);
+    IAP::setListener(this);
+    IAP::init();
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
+    CCMenuItemFont::setFontSize(48);
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    _coinCount = 0;
     
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    _txtCoin = Label::create("0", "fonts/Marker Felt.ttf", 32);
+    _txtCoin->setAnchorPoint(cocos2d::Vec2(0, 0));
+    _txtCoin->setPosition(cocos2d::Vec2(800, 500));
+    addChild(_txtCoin);
+    
+    CCMenuItemFont *item = CCMenuItemFont::create("Load", this, menu_selector(HelloWorld::onRequestIAP));
+    item->setPosition(0, 0);
+    CCMenuItemFont *item2 = CCMenuItemFont::create("Restore", this, menu_selector(HelloWorld::onRestoreIAP));
+    item->setPosition(0, 100);
+    
+    CCMenu *menu = CCMenu::create(item, item2, NULL);
+    menu->setAnchorPoint(cocos2d::Vec2(0, 0));
+    menu->setPosition(cocos2d::Vec2(200, 400));
+    addChild(menu);
+    
+    _iapMenu = CCMenu::create();
+    _iapMenu->setPosition(winWidth/2, 200);
+    addChild(_iapMenu);
+    
+    //Close menu
+    CCMenuItemFont* close_menu_item = CCMenuItemFont::create("Close", this, menu_selector(HelloWorld::menuCloseCallback));
+    CCMenu *close_menu = CCMenu::create(close_menu_item, NULL);
+    close_menu->setAnchorPoint(cocos2d::Point(0, 0));
+    close_menu->setPosition(cocos2d::Point(800, 100));
+    addChild(close_menu);
+    
+    Product test;
+    test.name = "remove_ads";
+    
+    _products.push_back(test);
+    updateIAP(_products);
     
     return true;
 }
 
+void HelloWorld::onShowAds(cocos2d::Ref *sender)
+{
+    CCLOG("Show Ads");
+}
+
+void HelloWorld::onRequestIAP(cocos2d::Ref* sender)
+{
+    IAP::refresh();
+}
+
+void HelloWorld::onRestoreIAP(cocos2d::Ref* sender)
+{
+    IAP::restore();
+}
+
+void HelloWorld::onIAP(cocos2d::Ref *sender)
+{
+    auto btn = static_cast<Node*>(sender);
+    Product* p = (Product*)btn->getUserData();
+    
+    CCLOG("Start IAP %s", p->name.c_str());
+    IAP::purchase(p->name);
+}
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+    MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+    
     Director::getInstance()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
 }
+
+void HelloWorld::onInitialized(bool ok)
+{
+    CCLOG("%s : %d", __func__, ok);
+}
+
+void HelloWorld::onSuccess(const Product &p)
+{
+    if (p.name == "coin_package") {
+        _coinCount += 1000;
+        _txtCoin->setString(tostr(_coinCount));
+    }
+    else if (p.name == "coin_package2") {
+        _coinCount += 5000;
+        _txtCoin->setString(tostr(_coinCount));
+    }
+    else if (p.name == "remove_ads") {
+        CCLOG("Remove Ads");
+    }
+    
+    CCLOG("Purchase Success: %s", p.id.c_str());
+}
+
+void HelloWorld::onFailure(const Product &p, const std::string &msg)
+{
+    CCLOG("Purchase Failed: %s", msg.c_str());
+}
+
+void HelloWorld::onCanceled(const Product &p)
+{
+    CCLOG("Purchase Canceled: %s", p.id.c_str());
+}
+
+void HelloWorld::onRestored(const Product& p)
+{
+    CCLOG("Purchase Restored: %s", p.name.c_str());
+}
+
+void HelloWorld::updateIAP(const std::vector<sdkbox::Product>& products)
+{
+    //
+    _iapMenu->removeAllChildren();
+    _products = products;
+    
+    int posX = 0;
+    int posY = 0;
+    
+    for (int i=0; i < _products.size(); i++)
+    {
+        CCLOG("IAP: ========= IAP Item =========");
+        CCLOG("IAP: Name: %s", _products[i].name.c_str());
+        CCLOG("IAP: ID: %s", _products[i].id.c_str());
+        CCLOG("IAP: Title: %s", _products[i].title.c_str());
+        CCLOG("IAP: Desc: %s", _products[i].description.c_str());
+        CCLOG("IAP: Price: %s", _products[i].price.c_str());
+        CCLOG("IAP: Price Value: %f", _products[i].priceValue);
+        
+#if COCOS2D_VERSION >= 196608
+        auto btn = ui::Button::create("button.png");
+        //        btn->setTitleText(_products[i].name);
+        btn->addClickEventListener(CC_CALLBACK_1(HelloWorld::onIAP, this));
+#else
+        auto btn = CCMenuItemFont::create(_products[i].name.c_str(), CC_CALLBACK_1(HelloWorld::onIAP, this) );
+#endif
+        btn->setColor(Color3B::WHITE);
+        btn->setUserData(&_products[i]);
+        btn->setPosition(CCPoint(posX, posY));
+        _iapMenu->addChild(btn);
+        posY += 50;
+    }
+}
+
+void HelloWorld::onProductRequestSuccess(const std::vector<Product> &products)
+{
+    updateIAP(products);
+}
+
+void HelloWorld::onProductRequestFailure(const std::string &msg)
+{
+    CCLOG("Fail to load products");
+}
+
+void HelloWorld::onRestoreComplete(bool ok, const std::string &msg)
+{
+    CCLOG("%s:%d:%s", __func__, ok, msg.data());
+}
+
---
+++ cpp/art/cocosstudio/MainScene.csd
@@ -0,0 +1,93 @@
+<GameProjectFile>
+  <PropertyGroup Type="Scene" Name="MainScene" ID="8df432b8-3b51-44f2-a7d8-5c74a1631983" Version="2.3.1.2" />
+  <Content ctype="GameProjectContent">
+    <Content>
+      <Animation Duration="0" Speed="1.0000" />
+      <ObjectData Name="Scene" ctype="GameNodeObjectData">
+        <Size X="960.0000" Y="640.0000" />
+        <Children>
+          <AbstractNodeData Name="background_1" ActionTag="353644068" Tag="9" IconVisible="False" RightMargin="-64.0000" ctype="SpriteObjectData">
+            <Size X="1024.0000" Y="640.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="512.0000" Y="320.0000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.5333" Y="0.5000" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <FileData Type="Normal" Path="background.jpg" Plist="" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="Button_1" ActionTag="6" Tag="4" IconVisible="False" HorizontalEdge="RightEdge" VerticalEdge="TopEdge" LeftMargin="977.5000" RightMargin="-57.5000" TopMargin="10.5000" BottomMargin="589.5000" TouchEnable="True" FontSize="14" Scale9Width="40" Scale9Height="40" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="ButtonObjectData">
+            <Size X="40.0000" Y="40.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="997.5000" Y="609.5000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="1.0391" Y="0.9523" />
+            <PreSize X="0.0000" Y="0.0000" />
+            <TextColor A="255" R="65" G="65" B="70" />
+            <DisabledFileData Type="Normal" Path="iphone/CloseNormal.png" Plist="" />
+            <PressedFileData Type="Normal" Path="iphone/CloseSelected.png" Plist="" />
+            <NormalFileData Type="Normal" Path="iphone/CloseNormal.png" Plist="" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="btnLoad" ActionTag="11" Tag="6" IconVisible="False" HorizontalEdge="LeftEdge" VerticalEdge="TopEdge" LeftMargin="33.0000" RightMargin="677.0000" TopMargin="26.5000" BottomMargin="513.5000" TouchEnable="True" FontSize="36" ButtonText="Load" Scale9Enable="True" LeftEage="5" RightEage="5" TopEage="5" BottomEage="5" Scale9OriginX="5" Scale9OriginY="5" Scale9Width="92" Scale9Height="30" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="ButtonObjectData">
+            <Size X="250.0000" Y="100.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="158.0000" Y="563.5000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.1646" Y="0.8805" />
+            <PreSize X="0.2604" Y="0.1563" />
+            <TextColor A="255" R="255" G="255" B="255" />
+            <DisabledFileData Type="Normal" Path="button.png" Plist="" />
+            <PressedFileData Type="Normal" Path="button_highlight.png" Plist="" />
+            <NormalFileData Type="Normal" Path="button.png" Plist="" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="btnRestore" ActionTag="-198921295" Tag="5" IconVisible="False" HorizontalEdge="LeftEdge" VerticalEdge="TopEdge" LeftMargin="32.0000" RightMargin="678.0000" TopMargin="138.5000" BottomMargin="401.5000" TouchEnable="True" FontSize="36" ButtonText="Restore" Scale9Enable="True" LeftEage="5" RightEage="5" TopEage="5" BottomEage="5" Scale9OriginX="5" Scale9OriginY="5" Scale9Width="92" Scale9Height="30" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="ButtonObjectData">
+            <Size X="250.0000" Y="100.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="157.0000" Y="451.5000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.1635" Y="0.7055" />
+            <PreSize X="0.2604" Y="0.1563" />
+            <TextColor A="255" R="255" G="255" B="255" />
+            <DisabledFileData Type="Normal" Path="button.png" Plist="" />
+            <PressedFileData Type="Normal" Path="button_highlight.png" Plist="" />
+            <NormalFileData Type="Normal" Path="button.png" Plist="" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="txtCoin" ActionTag="-1100286847" Tag="7" IconVisible="False" HorizontalEdge="RightEdge" VerticalEdge="TopEdge" LeftMargin="791.0000" RightMargin="89.0000" TopMargin="14.5000" BottomMargin="583.5000" IsCustomSize="True" FontSize="36" LabelText="0" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="TextObjectData">
+            <Size X="80.0000" Y="42.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="831.0000" Y="604.5000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.8656" Y="0.9445" />
+            <PreSize X="0.0833" Y="0.0656" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="Text_2" ActionTag="-267284318" Tag="8" IconVisible="False" HorizontalEdge="RightEdge" VerticalEdge="TopEdge" LeftMargin="695.0000" RightMargin="181.0000" TopMargin="13.5000" BottomMargin="584.5000" FontSize="36" LabelText="Coin:" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="TextObjectData">
+            <Size X="84.0000" Y="42.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="737.0000" Y="605.5000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.7677" Y="0.9461" />
+            <PreSize X="0.0000" Y="0.0000" />
+          </AbstractNodeData>
+          <AbstractNodeData Name="btnAds" ActionTag="-228087084" Tag="8" IconVisible="False" HorizontalEdge="LeftEdge" VerticalEdge="TopEdge" LeftMargin="31.0000" RightMargin="679.0000" TopMargin="254.0000" BottomMargin="286.0000" TouchEnable="True" FontSize="36" ButtonText="Show Ad" Scale9Enable="True" LeftEage="5" RightEage="5" TopEage="5" BottomEage="5" Scale9OriginX="5" Scale9OriginY="5" Scale9Width="92" Scale9Height="30" ShadowOffsetX="2.0000" ShadowOffsetY="-2.0000" ctype="ButtonObjectData">
+            <Size X="250.0000" Y="100.0000" />
+            <AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />
+            <Position X="156.0000" Y="336.0000" />
+            <Scale ScaleX="1.0000" ScaleY="1.0000" />
+            <CColor A="255" R="255" G="255" B="255" />
+            <PrePosition X="0.1625" Y="0.5250" />
+            <PreSize X="0.2604" Y="0.1563" />
+            <TextColor A="255" R="255" G="255" B="255" />
+            <DisabledFileData Type="Normal" Path="button.png" Plist="" />
+            <PressedFileData Type="Normal" Path="button_highlight.png" Plist="" />
+            <NormalFileData Type="Normal" Path="button.png" Plist="" />
+          </AbstractNodeData>
+        </Children>
+      </ObjectData>
+    </Content>
+  </Content>
+</GameProjectFile>
